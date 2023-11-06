b75a382c2bb249122d3fcb9d1cec2dab460ac971
brunomry <brunomadozzo@gmail.com>
2023-11-06 01:56:42 -0300

    Resolucion de ejercicio n°12 - tp2

--- JS/ejercicio12-tp2.js
+++ JS/ejercicio12-tp2.js
@@ -1,1 +1,10 @@
+document.write(`<main class="p-5"><h2 class="pb-3">Resolución de Ejercicio N°12</h2>`);
 
+const minimo = 1;
+const maximo = 99;
+
+let numeroAleatorio = Math.random() * (maximo - minimo) + minimo;
+
+document.write(`<p>Número aleatorio entre ${minimo} y ${maximo}: <span class="fw-bold">${numeroAleatorio}</span></p>`);
+
+document.write(`</main>`);
